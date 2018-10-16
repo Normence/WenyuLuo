f95f82c3b5b129454b43befaa411aa0d168d6d66
Wenyu Luo <normence@gmail.com>
2018-10-16 14:06:45 -0700

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => (
+  <header className="masthead d-flex">
+    <div className="container text-center my-auto">
+      <h1 className="mb-1">文里</h1>
+      <h3 className="mb-5">
+        <em>Wenyu Luo</em>
+      </h3>
+      <a className="btn btn-primary btn-xl js-scroll-trigger" href="#portfolio">
+        Discover
+      </a>
+    </div>
+    <div className="overlay" />
+  </header>
+);
+export default Header;
